6a45a5abf7c466ba7a7b51d236306aa579571e2e
ShermanDrake <shermandrake@comcast.net>
2015-10-06 16:44:35 -0600

    second commit

--- app.js
+++ app.js
@@ -7,8 +7,24 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.static(__dirname + '/public'))
 
+app.get('/', function(req, res){
+		// res.send('hello')
+
+res.sendFile('Seville.html', {root : './public/html'})
+
+
+})
+
+app.get('/:location', function(req, res){
+	res.sendFile(req.params.location + '.html', {root : './public/html'})
+
+})
+
+
+
 var port = 3000
 app.listen(port, function(){
 	console.log('server is listening on port ' + port)
 
 })
+
