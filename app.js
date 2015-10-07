61901a1cdd3666d744de6e805659506944798896
ShermanDrake <shermandrake@comcast.net>
2015-10-07 16:15:35 -0600

    wednesday commit

--- app.js
+++ app.js
@@ -15,10 +15,54 @@ res.sendFile('Seville.html', {root : './public/html'})
 
 })
 
-app.get('/:location', function(req, res){
-	res.sendFile(req.params.location + '.html', {root : './public/html'})
+app.get('/next', function(req, res, next){
+
+	res.json(req.query) 
+
 
 })
+
+app.get('/:location', function(req, res, next){
+
+	var pagelocation = ['capeVerde', 'straitOfMagellan', 'philippines', 'Seville', 'canaryIslands', 'Guam' ]
+
+		if (pagelocation.indexOf(req.params.location) != -1) {
+
+			res.sendFile(req.params.location + '.html', {root : './public/html'})
+		}
+		else {
+			// next(new Error(req.params.location + 'Is Not Found'))
+			next()
+		}
+
+
+
+
+
+
+
+
+
+	// res.sendFile(req.params.location + '.html', {root : './public/html'})
+
+	  // if ( req.params.location === 'error' ) {
+   //      next(new Error('my custom error'))
+   //  }
+   //  else {
+   //  	console.log(req.params.location)
+   //      res.sendFile(req.params.location + '.html', {root : './public/html'})
+   //  }
+
+
+})
+
+app.use(function(req, res) {
+
+  res.send('ERROR! ERROR! PAGE NOT FOUND!')
+
+})
+
+
 
 
 
